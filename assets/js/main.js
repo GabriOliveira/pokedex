52fe58f29e98d1ae9b8aaa5b55348eb39de82720
GabriOliveira <gabriel007og@gmail.com>
2024-11-17 20:52:57 -0300

    consumindo API pokeapi, utilizando javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
-console.log('Sucesso');
+
+
+
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+
+function convertPokemonToHtml(pokemon){
+    return `<li class="pokemon ${pokemon.type}" >
+        <span class="number">#0${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+          <ol class="types">
+           ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+          </ol>
+          <img src="${pokemon.photo}" alt="${pokemon.name}" />
+        </div>
+          </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList'); //pegamos a nossa lista em html
+
+pokeApi.getPokemons().then((pokemons = []) => {
+  pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
+
+})
+    
+
